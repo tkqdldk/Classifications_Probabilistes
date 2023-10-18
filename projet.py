288476f3b66d914f8cc34fa584fb75b7d12c0fa9
Samia Bouchaal <samia20bouchaal@gmail.com>
2023-10-18 15:54:41 +0200

    question1 moyenne

--- projet.py
+++ projet.py
@@ -2,7 +2,23 @@
 # Samia Bouchaal
 
 import utils
+import pandas as pd
 import numpy as np
 
+def getPrior(df):
+    """
+    Cette fonction calcule la probabilite à priori de la classe $1$ ainsi que l'intervalle de confiance
 
+    Parameters
+    ----------
+    df : DataFrame
 
+    Results
+    -------
+    Dict[estimation,
+    """
+    moyenne=df.loc[:,"target"].mean()
+
+    return moyenne
+
+getPrior(train.csv)
