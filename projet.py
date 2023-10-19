7437e858573334e2d41e5e96c0f96f42f7a91889
Samia Bouchaal <samia20bouchaal@gmail.com>
2023-10-19 12:17:00 +0200

    description de la fonction getPrior

--- projet.py
+++ projet.py
@@ -5,6 +5,9 @@ import utils
 import pandas as pd
 import numpy as np
 import math
+import utils
+
+
 
 def getPrior(df):
     """
@@ -12,22 +15,25 @@ def getPrior(df):
 
     Parameters
     ----------
-    df : DataFrame etudié
+        df : DataFrame
+            Base de donnees utilisee
 
     Results
     -------
-    Dict['estimation','min5pourcent','max5pourcent']
+        Dict[estimation,min5pourcent,max5pourcent]
+            Probabilite a priori et intervalle de confiance
     """
 
-    moyenne=df.loc[:,"target"].mean()
-    ecart_type=df.loc[:,"target"].std()
+    moyenne=df.loc[:,"target"].mean() #calcule la moyenne des valeurs de la colonne target
+    ecart_type=df.loc[:,"target"].std() # calcule l'ecart-type
 
-    me = 1.96*(ecart_type/math.sqrt(df.shape[0]))
+    me = 1.96*(ecart_type/math.sqrt(df.shape[0])) #marge d'erreur
 
     minpourcent = moyenne-me
 
     maxpourcent = moyenne+me
 
     return {'estimation' : moyenne, 'min5pourcent' : minpourcent , 'max5pourcent': maxpourcent}
+
 
 
