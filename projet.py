d51830d06ac106096645afe527826f585c77d10d
Itchir Maissa <98089569+ItchirMaissa@users.noreply.github.com>
2023-10-25 19:34:32 +0200

    Update projet.py

--- projet.py
+++ projet.py
@@ -105,4 +105,83 @@ class AprioriClassifier(utils.AbstractClassifier):
        
         return dico
 
+# Le resulatat de la fonction est un dictionnaire des dictionnaires
+#  linstruction df['target'].unique() renvoie les valeurs uniques de la colonne target dans le dataFrame qui 
+#  pour chaque paire (target, attribut)  on calcule la probabilite conditionnelle P(attribut=a∣target=t) 
+def P2D_l(df,attr):
+    resultat = dict()
+    for target in df['target'].unique():  
+        tmp = dict()                      # On initialise un dictionnaire temporire pour chaque valeur
+         for val_attr in df[attr].unique():  # On parcourt les valeurs uniques de la colonne specifiee par 'attr'
+            tmp[val_attr] = 0
+        res[target] = tmp # le dictionnaire initiale contient la valeur du target ans le premier champs et le dictionnaire temporaire dans le 2 eme champs
+        group = df.groupby(["target", attr]).groups # regrouper  les données en fonction des combinaisons de la variable cible 'target'attribut
+    for t, val in group: # arcourt les combinaisons de la variable cible et de l'attribut
+        res[t][val] = len(group[(t, val)])  #Compte le nombre d'occurrences pour cette combinaison et l'assigne au dictionnaire de résultats res
+   
+    group = df.groupby(["target"])[attr].count()   # Compte le nombre d'occurrences de chaque valeur de l'attribut pour chaque classe de la variable cible
+    
+    
+    for target in res.keys():  # Parcourt les clés (valeurs de l'attribut) dans le dictionnaire associé à la valeur de la variable cible 'target'
+        for val_attribut in res[target].keys():
+            res[target][val_attribut] /= group[target] #  Calcule la probabilité conditionnelle en divisant le nombre d'occurrences par le nombre total d'occurrences de cette classe de la variable cible
 
+    return res
+
+
+
+# calcule les probabilités conditionnelles pour chaque combinaison de l'attribut et de la variable cible. 
+  #  Le résultat est renvoyé sous forme d'un dictionnaire de dictionnaires.
+    
+def P2D_p(df, attr):
+    res = dict()
+    for val_attr in df[attr].unique():
+        tmp = dict()
+        for val_targ in df['target'].unique():
+            tmp[val_targ] = 0
+        res[val_attr] = tmp
+       
+    group = df.groupby([attr, "target"]).groups
+    for t, val in group:
+        res[t][val] = len(group[(t, val)])
+
+    group = df.groupby([attr])['target'].count()
+   
+    for val_attr in res.keys():
+        for val_targ in res[val_attr].keys():
+            res[val_attr][val_targ] /= group[val_attr]
+    return res  
+
+
+
+
+class ML2DClassifier(APrioriClassifier):
+    def __init__(self, df, attr):
+        self.attr = attr
+        self.dico = P2D_l(df, attr)
+
+    def estimClass(self, attrs):
+        val_attr = attrs[self.attr]
+        classe = 0
+        proba = 0
+        for i in self.dico.keys():
+            if self.dico[i][val_attr] >= proba :
+                classe = i
+                proba = self.dico[i][val_attr]
+        return classe
+    
+    
+class MAP2DClassifier(APrioriClassifier):
+    def __init__(self, df, attr):
+        self.attr = attr
+        self.dico = P2D_p(df, attr)
+       
+    def estimClass(self, attrs):
+        val_attr = attrs[self.attr]
+        classe = 0
+        proba = 0
+        for i in self.dico[val_attr].keys():
+            if self.dico[val_attr][i] >= proba :
+                classe = i
+                proba = self.dico[val_attr][i]
+        return classe
